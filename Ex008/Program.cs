00ab22fc9d3b33da30d265731ac3a07b81e08a23
edu <eduardo.glira@senacsp.edu.br>
2025-01-16 22:26:24 -0300

    atualização

---
+++ Ex008/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ex008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            string resultado;
+
+            Console.WriteLine("Digite um número");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("é par");
+
+            }
+            else 
+            {
+                Console.WriteLine("é impar");
+            }
+
+           
+        
+        }
+    }
+}
