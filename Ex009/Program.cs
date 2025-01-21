cfc1ef9eeb948ed69b0818d2381cf4e547ed6036
edu <eduardo.glira@senacsp.edu.br>
2025-01-21 19:30:23 -0300

    Ex009

---
+++ Ex009/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ladoA,ladoB,ladoC;
+
+            Console.WriteLine("Digite o valor do Lado A: ");
+            ladoA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do Lado B: ");
+            ladoB = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do Lado C: ");
+            ladoC = Convert.ToInt32(Console.ReadLine());
+
+            if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoB))
+            {
+                Console.WriteLine("Os valores informados formam um triângulo.");
+            }
+            else
+            {
+                Console.WriteLine("Os Valores informados não formam um triângulo.");
+            }
+        }
+    }
+}
