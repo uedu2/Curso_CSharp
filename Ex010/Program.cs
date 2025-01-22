eccd9be6069d71f1a22d3dde278878109213879f
edu <eduardo.glira@senacsp.edu.br>
2025-01-21 21:38:09 -0300

    exercicios

---
+++ Ex010/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ex010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sal, aum;
+
+            Console.WriteLine("Salário:");
+            sal = Convert.ToDouble(Console.ReadLine());
+
+            if (sal > 2500)
+            {
+                aum = (sal + (sal * 0.05));
+                Console.WriteLine("seu aumento é de: {0}", aum);
+
+            }
+            else if (sal > 1250)
+            {
+                aum = (sal + (sal * 0.1));
+                Console.WriteLine("seu aumento é de: {0}", aum);
+            }
+            else
+            {
+                aum = (sal + (sal * 0.15));
+                Console.WriteLine("seu aumento é de: {0}", aum);
+            }
+        } 
+    }
+}
---
+++ Ex011/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO.Pipes;
+
+namespace Ex011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sal, val, pres;
+
+            Console.WriteLine("salário");
+            sal = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Valor da casa");
+            val = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Prestações em anos");
+            pres = Convert.ToDouble(Console.ReadLine());
+
+            if (val / (pres * 12) < sal * 0.3)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
+        }
+    }
+}
