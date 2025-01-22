17149e545362ac4e0b512d5acf71340ee1a81e60
edu <eduardo.glira@senacsp.edu.br>
2025-01-21 22:04:35 -0300

    18 anos

---
+++ Ex013/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ex013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+
+            Console.WriteLine("Digite");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            if (idade == 18)
+            {
+                Console.WriteLine("Chegou a sua hora!");
+            }
+            else if (idade != 18) 
+            {
+             
+                if (idade < 18)
+                {
+                    Console.WriteLine("faltam {0} anos",18 - idade);
+                }
+                else 
+                {
+                    Console.WriteLine("você se alistou a {0} anos!", idade-18);
+                }
+                
+            }
+        }
+    }
+}
