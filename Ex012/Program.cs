6f5ac6b8dff93ee62ef089ed128d0f4a2dee80a5
edu <eduardo.glira@senacsp.edu.br>
2025-01-21 21:43:45 -0300

    sim

---
+++ Ex012/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ex012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1, var2;
+
+            Console.WriteLine("Digite o Número 1:");
+            var1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o Número 2:");
+            var2 = Convert.ToInt32(Console.ReadLine());
+
+            if (var1 > var2)
+            {
+                Console.WriteLine("o Valor 1 é maior");
+            }
+            else if (var1 < var2)
+            {
+                Console.WriteLine("o Valor 2 é maior");
+            }
+            else
+            {
+                Console.WriteLine("nenhum dos dois é maior que o outro");
+            }
+        }
+    }
+}
