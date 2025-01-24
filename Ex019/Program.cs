d52dc74fb0655d2f3bff3137cf0e5ea4f3929be0
edu <eduardo.glira@senacsp.edu.br>
2025-01-23 21:17:19 -0300

    att

---
+++ Ex019/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Digite um número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++) 
+            {
+                Console.WriteLine("{0}X{1}={2}",num, i, num*i);
+            }
+        }
+    }
+}
---
+++ Ex020/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
