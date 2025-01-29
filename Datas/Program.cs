13e48ac8eaa1f6c7ef78b24602d3b18373a2a10c
edu <eduardo.glira@senacsp.edu.br>
2025-01-28 22:26:08 -0300

    salvar

--- Datas/Program.cs
+++ Datas/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
 
-            string[] meses = { "Janeiro","Fevereiro","Março","Abril","Maio", "Junho", "Julho",
+            /* string[] meses = { "Janeiro","Fevereiro","Março","Abril","Maio", "Junho", "Julho",
                 "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
             int[] dias = {31,28,31,30,31,30,31,31,30,31,30,31};
 
@@ -14,6 +14,19 @@
                 Console.WriteLine("o mês de " + meses[i] + " tem " + dias[i] + " dias.");
             }
 
+            string[] semama = {"Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"};
+            foreach (string dia in semama) 
+            {
+                Console.WriteLine(dia);
+            }
+            */
+
+            int[] numeros = { 10,45 ,22,50,6, -84,-2,91,-32 };
+
+            Console.WriteLine(numeros.First());
+            Console.WriteLine(numeros.Last());
+            Console.WriteLine(numeros.Max());
+            Console.WriteLine(numeros.Min());
         }
     }
 }
---
+++ VetoresFrutas/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace VetoresFrutas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] frutas = {"Banana","Baunilha","Algodão Doce","Kiwi","Uva","Pastel Azul","Milho","Chili-Dog","Jaca","Graviola"};
+            double[] preco = {98.38,91.3,3.99,4.67,5777.49,698.67,79.65,458.70,9086.24,105.99 };
+            for (int i = 0; i < frutas.Length; i++) 
+            {
+                Console.WriteLine(frutas[i] + " R$" + preco[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("o maior preço é " + preco.Max());
+            Console.WriteLine("o maior preço é " + preco.Min());
+            Console.WriteLine("o maior preço é " + preco.Average());
+        }
+    }
+}
