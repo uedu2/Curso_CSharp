9a87e5c17e527dbb3c05e873ee05bc6d2550b13e
edu <eduardo.glira@senacsp.edu.br>
2025-01-30 21:55:06 -0300

    escrever

---
+++ Contador/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Contador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void contador(int inicio, int fim, int passo)
+        {
+
+        }
+        static void escrever(string texto)
+        {
+            int tam = texto.Length + 4;
+            string linha = new('=', tam);
+            string textocent = texto.PadLeft(tam - 2).PadRight(tam - 2);
+            Console.WriteLine(linha);
+            Console.WriteLine(textocent);
+            Console.WriteLine(linha);
+        }
+    }
+}
---
+++ Método/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Método
+{
+    internal class Program
+    {
+        static void escrever(string texto)
+        {
+            int tam = texto.Length + 4;
+            string linha = new('=',tam);
+            string textocent =texto.PadLeft(tam -2).PadRight(tam -2);
+            Console.WriteLine(linha);
+            Console.WriteLine(textocent);
+            Console.WriteLine(linha);
+        }
+
+        static void Main(string[] args)
+        {
+            escrever("Eduardo Gomes De Lira");
+            Console.WriteLine();
+            escrever("Curso C#");
+            Console.WriteLine();
+            escrever("Olá Mundo");
+            Console.WriteLine();
+        }
+
+    }
+}
