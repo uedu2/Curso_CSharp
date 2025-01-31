d39e557d408f0fb8624db432688e751ec24d4805
edu <eduardo.glira@senacsp.edu.br>
2025-01-30 21:28:44 -0300

    Triângulo

--- AreaTriangulo/Program.cs
+++ AreaTriangulo/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("=================");
+            Console.WriteLine("Área do Triângulo");
+            Console.WriteLine("=================");
+            Console.WriteLine();
+
+            Console.Write("Digite a altura: ");
+            int altura = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            
+            Console.Write("Digite a base: ");
+            int Base = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            areatriangulo(altura, Base);
+
+        }
+        static void areatriangulo(int h, int b)
+        {
+            int a = (h * b) / 2;
+            Console.WriteLine($"a área do triângulo de altura {h} e base {b} é igual a: {a}");
         }
     }
 }
